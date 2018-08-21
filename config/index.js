082cf1e9c14c61ddcd5a65c4bfda3d46a26bc51d
kevinten10 <596823919@qq.com>
2018-08-21 16:05:14 +0800

    version1.1.3   --2018.8.21   --nginx部署配置多vue项目   --测试跨域访问

--- config/index.js
+++ config/index.js
@@ -17,6 +17,7 @@ module.exports = {
         pathRewrite: {
           '^/api': '/'         //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://10.1.5.11:8080/xxx/duty?time=2017-07-07 14:57:22'，直接写‘/api/xxx/duty?time=2017-07-07 14:57:22’即可
         }
+      }
     },
 
     // Various Dev Server settings
